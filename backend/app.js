381418bc9dda2bd7686f956561c4c53db0883c73
rayalima-cell <ray_a_lima@estudante.sesisenai.org.br>
2026-09-09 12:07:11 -0300

    08

--- backend/app.js
+++ backend/app.js
@@ -6,3 +6,34 @@ const server = express();
 
 server.use(cors());
 server.use(express.json());
+
+const port = 3025;
+
+server.get("/produtos", (req, res) => {
+    const sql = 'SELECT * FROM PRODUTO';
+
+    connection.query(sql, (erro, resultados) => {
+        if(erro){
+            console.log('Erro ao buscar produtos: ', erro);
+            res.status(500).send('Erro ao buscar produtos');
+            return;
+        }
+        res.send(resultados);
+    });
+});
+
+server.get("/produtos/ordenados", (req, res) => {
+    const sql = 'SELECT * FROM PRODUTO ORDER BY nome ASC';
+
+    connection.query(sql, (erro, resultados) => {
+        if(erro){
+           return res.status(500).json({ erro: erro.message });
+        }
+        return res.json(resultados);
+    });
+})
+
+
+server.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
---
+++ backend/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'saep_db1'
+});
+
+connection.connect((erro) =>{
+    if(erro){
+        console.log('Erro ao conectar com o banco de dados: ', erro);
+        return;
+    }
+    console.log('Banco de dados conectado com sucesso!');
+});
+
+module.exports = connection;
